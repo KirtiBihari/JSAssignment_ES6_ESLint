a402eb7698622c3f9765a0b6286c65585655ad7a
Kirti <k.sundar.info@gmail.com>
2018-08-17 15:56:10 +0530

    update run.sh

--- run.sh
+++ run.sh
@@ -4,18 +4,12 @@
 
 args="$@"
 
-args="$@ -p 80"
+args="$@ -p 3000"
 
 file=/db.json
 if [ -f $file ]; then
     echo "Found db.json, trying to open"
     args="$args db.json"
-fi
-
-file=/file.js
-if [ -f $file ]; then
-    echo "Found file.js seed file, trying to open"
-    args="$args file.js"
 fi
 
 json-server $args
